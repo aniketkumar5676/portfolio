db21db6a7c3ed93f408daf417ff2f397a1664014
aniketkumar5676 <114988355+aniketkumar5676@users.noreply.github.com>
2022-10-14 13:19:18 +0530

    Delete ClassRoomDay26_ClassTest directory

--- ClassRoomDay26_ClassTest/chattingSystem/chattingSystem/src/main/java/com/chatting/chattingSystem/ExceptionHandler/UserNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.chatting.chattingSystem.ExceptionHandler;
-
-public class UserNotFound extends  Exception {
-    public UserNotFound(String message) {
-        super(message);
-    }
-}
--- ClassRoomDay26_ClassTest/chattingSystem/chattingSystem/src/main/java/com/chatting/chattingSystem/Repo/UserCredentialsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.chatting.chattingSystem.Repo;
-
-import com.chatting.chattingSystem.Entity.UserCredentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCredentialsRepo extends JpaRepository<UserCredentials,Integer> {
-    UserCredentials findByUsername(String username);
-}
